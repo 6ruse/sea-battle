be0a627f68e2d0be5c779421fcfac0d7f3e2afb5
6ruse <BKNovikov@GMail.com>
2022-12-25 22:32:24 +0700

    C2.8. Итоговое задание
    дополнительные коммиты я думаю не нужны.

---
+++ classGame.py
@@ -0,0 +1,90 @@
+from classShip import Ship
+from classesPlayer import AI, User
+from classBoard import Board
+from classDot import Dot
+from random import randint
+from classesBoardException import BoardWrongShipException
+
+class Game:
+    def __init__(self, size=6):
+        self.lens = [3, 2, 2, 1, 1, 1, 1]
+        self.size = size
+        pl = self.random_board()
+        co = self.random_board()
+        co.hid = True
+
+        self.ai = AI(co, pl)
+        self.us = User(pl, co)
+
+    def random_board(self):
+        board = None
+        while board is None:
+            board = self.random_place()
+        return board
+
+    def random_place(self):
+        board = Board(size=self.size)
+        attempts = 0
+        for l in self.lens:
+            while True:
+                attempts += 1
+                if attempts > 2000:
+                    return None
+                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), l, randint(0, 1))
+                try:
+                    board.add_ship(ship)
+                    break
+                except BoardWrongShipException:
+                    pass
+        board.begin()
+        return board
+
+    def greet(self):
+        print("-------------------")
+        print("  Приветсвуем вас  ")
+        print("      в игре       ")
+        print("    морской бой    ")
+        print("-------------------")
+        print(" формат ввода: x y ")
+        print(" x - номер строки  ")
+        print(" y - номер столбца ")
+
+    def print_boards(self):
+        print("-" * 20)
+        print("Доска пользователя:")
+        print(self.us.board)
+        print("-" * 20)
+        print("Доска компьютера:")
+        print(self.ai.board)
+
+    def loop(self):
+        num = 0
+        while True:
+            self.print_boards()
+            if num % 2 == 0:
+                print("-" * 20)
+                print("Ходит пользователь!")
+                repeat = self.us.move()
+            else:
+                print("-" * 20)
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.defeat():
+                self.print_boards()
+                print("-" * 20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board.defeat():
+                self.print_boards()
+                print("-" * 20)
+                print("Компьютер выиграл!")
+                break
+            num += 1
+
+    def start(self):
+        self.greet()
+        self.loop()
---
+++ main.py
@@ -0,0 +1,4 @@
+from classGame import Game
+
+g = Game()
+g.start()
